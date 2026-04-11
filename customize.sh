0ef5df23986cabea36c80c45a8fc18e5a5df0f81
Raino <151395979+Rainophey@users.noreply.github.com>
2026-04-11 13:07:51 +0800

    Implement device code extraction in customize.sh
    not end yet

--- customize.sh
+++ customize.sh
@@ -1,3 +1,15 @@
-echo 
-echo Done?
-echo
+DEIVCECODE=$(getprop ro.product.device)
+ui_print "- Your device's code is $DEVICECODE."
+
+MOD_TARGET_DIR="$MODPATH/system/product/etc/device_feature"
+
+SRC_FILE="/system/product/etc/device_feature/$CODENAME.xml"
+
+if [ -f "$SRC_FILE" ]; then
+    ui_print "- Extracting $DEIVCECODE.xml..."
+    mkdir -p "$MOD_TARGET_DIR"
+    cp "$SRC_FILE" "$MOD_TARGET_DIR/$DEIVCECODE.xml"
+else
+    # 如果没找到对应文件，中止安装并报错
+    abort "! Error: Device feature file not found for $DEIVCECODE"
+fi
