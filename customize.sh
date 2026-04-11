ab0b16751ae878f53320ddff3749a334ff4f6e12
Raino <151395979+Rainophey@users.noreply.github.com>
2026-04-12 01:10:21 +0800

    Complete all code

--- customize.sh
+++ customize.sh
@@ -1,15 +1,46 @@
-DEIVCECODE=$(getprop ro.product.device)
+#1.Extra file
+
+DEVICECODE=$(getprop ro.product.device)
+ui_print "-"
 ui_print "- Your device's code is $DEVICECODE."
 
 MOD_TARGET_DIR="$MODPATH/system/product/etc/device_feature"
-
-SRC_FILE="/system/product/etc/device_feature/$CODENAME.xml"
+SRC_FILE="/system/product/etc/device_feature/$DEVICECODE.xml"
 
 if [ -f "$SRC_FILE" ]; then
-    ui_print "- Extracting $DEIVCECODE.xml..."
+    ui_print "- Extracting $DEVICECODE.xml"
     mkdir -p "$MOD_TARGET_DIR"
-    cp "$SRC_FILE" "$MOD_TARGET_DIR/$DEIVCECODE.xml"
+    cp "$SRC_FILE" "$MOD_TARGET_DIR/$DEVICECODE.xml"
 else
-    # 如果没找到对应文件，中止安装并报错
-    abort "! Error: Device feature file not found for $DEIVCECODE"
+    abort "! Error: Device feature file not found for $DEVICECODE"
 fi
+ui_print "- then..."
+
+#2.Change feature bool
+
+TARGET_FILE="$MOD_TARGET_DIR/$DEVICECODE.xml"
+
+change_bool() {
+    local feature=$1
+    local bool=$2
+    local file=$3
+    local comment=$4
+
+    if grep -q "name=\"$feature\"" "$file"; then
+        ui_print "-"
+        ui_print "- Feature $feature exists, changing bool..."
+        sed -i "s|<bool name=\"$feature\">.*</bool>|<bool name=\"$feature\">$bool</bool>|g" "$file"
+    else
+        ui_print "-"
+        ui_print "- Feature $feature not found, adding new entry..."
+        if [ ! -z "$comment" ]; then
+            sed -i "/<\/features>/i \    <!-- $comment -->" "$file"
+        fi
+        sed -i "/<\/features>/i \    <bool name=\"$feature\">$bool</bool>" "$file"
+    fi
+}
+
+change_bool "support_aod_fullscreen" "true" "$TARGET_FILE" "whether the device supports aod fullscreen mode"
+
+ui_print "  "
+ui_print "- Everything done!"
